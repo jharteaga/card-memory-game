917cdd8eae466af92ba2e661944f7d6ba4b4fb6e
jharteaga <jh.arteaga09@gmail.com>
2020-04-02 00:40:47 -0700

    Added behavior to keep pairs found & flip if not

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import { images } from './cards';
 
 const cards = document.querySelectorAll('.container__cards__card');
+let cardSelected1,
+  cardSelected2,
+  cardsNotPlaying = [],
+  count = 0;
 
 init();
-addCardEvents(cards);
 
 //Initiate Game
 function init() {
@@ -16,24 +19,110 @@ function init() {
     card.className = 'container__cards__card__img';
     cards[i].appendChild(card);
   }
+
+  addCardEvents(cards);
+}
+
+//Block Board Game
+function blockedBoard() {
+  cardsNotPlaying = [...cards].filter(
+    (card) => card !== cardSelected1 && card !== cardSelected2
+  );
+
+  cardsNotPlaying.forEach((card) => {
+    card.classList.add('container__cards__card--blocked');
+  });
+}
+
+//Enable Board Game
+function enableBoard() {
+  cardsNotPlaying.forEach((card) => {
+    card.classList.remove('container__cards__card--blocked');
+  });
+}
+
+//Assign pair to check them
+function assignPairToCheck(element) {
+  if (count === 0) {
+    cardSelected1 = element;
+    count++;
+  } else if (count === 1) {
+    cardSelected2 = element;
+    blockedBoard();
+    checkPair();
+  }
+}
+
+//Check pairs
+function checkPair() {
+  const card1 = images.filter(
+    (img, index) => `card${index + 1}` === cardSelected1.id
+  );
+  const card2 = images.filter(
+    (img, index) => `card${index + 1}` === cardSelected2.id
+  );
+
+  if (card1[0].image === card2[0].image) {
+    pairFound();
+  } else {
+    incorrectPair();
+  }
+}
+
+//Pair Found
+function pairFound() {
+  cardSelected1.classList.remove('container__cards__card--flip-front');
+  cardSelected1.classList.remove('container__cards__card--flip-back');
+
+  cardSelected2.classList.remove('container__cards__card--flip-front');
+  cardSelected2.classList.remove('container__cards__card--flip-back');
+
+  cardSelected1.classList.add('container__cards__card--found');
+  cardSelected2.classList.add('container__cards__card--found');
+
+  reset();
+}
+
+//Incorrect pair
+function incorrectPair() {
+  setTimeout(() => {
+    flipEvent(cardSelected1);
+    flipEvent(cardSelected2);
+  }, 800);
 }
 
 //Add events listeners
 function addCardEvents(elements) {
   elements.forEach((element) => {
     element.addEventListener('click', () => {
-      element.classList.toggle('container__cards__card--flip-front');
-      element.classList.toggle('container__cards__card--flip-back');
-
-      setTimeout(() => {
-        if (element.classList.contains('container__cards__card--flip-back')) {
-          element.childNodes[1].style.visibility = 'hidden';
-          element.childNodes[3].style.visibility = 'visible';
-        } else {
-          element.childNodes[1].style.visibility = 'visible';
-          element.childNodes[3].style.visibility = 'hidden';
-        }
-      }, 360);
+      flipEvent(element);
     });
   });
+}
+
+//Flip Event
+function flipEvent(element) {
+  element.classList.toggle('container__cards__card--flip-front');
+  element.classList.toggle('container__cards__card--flip-back');
+
+  setTimeout(() => {
+    if (element.classList.contains('container__cards__card--flip-back')) {
+      element.childNodes[1].style.visibility = 'hidden';
+      element.childNodes[3].style.visibility = 'visible';
+      assignPairToCheck(element);
+    } else {
+      element.childNodes[1].style.visibility = 'visible';
+      element.childNodes[3].style.visibility = 'hidden';
+      reset();
+    }
+  }, 360);
+}
+
+//Reset event
+function reset() {
+  count = 0;
+  enableBoard();
+  cardSelected1 = null;
+  cardSelected2 = null;
+  cardsNotPlaying = [];
 }
